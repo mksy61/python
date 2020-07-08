538f43faa81a3b0830cb8c8407c22deede286472
murataksoy61 <44951062+murataksoy61@users.noreply.github.com>
2020-07-08 23:06:38 +0300

    ..

---
+++ learn/print.py
@@ -0,0 +1,15 @@
+print("Merhaba, python")
+x= 5 
+y= 3.12
+z="Murat"
+print(z)
+
+print(type(x))
+x="aa"
+print(type(x))
+print(type(y))
+print(type(z))
+
+print("M","M","M", sep="*", end="\n\n\n")
+z="Murat"
+print(z)
