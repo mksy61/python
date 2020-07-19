8deaa83df44d5fb5175e351af94b7e3764b0a66f
murataksoy61 <44951062+murataksoy61@users.noreply.github.com>
2020-07-19 16:27:48 +0300

    ..

--- learn/cc/generator.py
+++ learn/cc/generator.py
@@ -6,8 +6,11 @@ def gen(n):
         yield i**2
 
 
-h = [i**2 for i in range(10000000)]
-g = gen(10000000)
+h = [i**2 for i in range(100000)]
+g = gen(100000)
 
 print(sys.getsizeof(g))
 print(sys.getsizeof(h))
+
+print(type(g))
+print(type(h))
