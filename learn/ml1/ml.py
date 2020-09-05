bb42ca6fc293f11870039298678bdb298798e87d
murataksoy61 <44951062+murataksoy61@users.noreply.github.com>
2020-09-05 14:24:04 +0300

    ..

--- learn/ml1/ml.py
+++ learn/ml1/ml.py
@@ -12,6 +12,7 @@ from sklearn.linear_model import GammaRegressor
 from sklearn.linear_model import PoissonRegressor
 from sklearn.linear_model import HuberRegressor
 from sklearn.linear_model import TweedieRegressor
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error
 
 df = pd.read_csv("train.csv")
@@ -94,6 +95,26 @@ val_predictions = model.predict(val_X)
 val_mae = mean_absolute_error(val_y, val_predictions)
 models[str(type(model))] = val_mae
 
+model = RandomForestRegressor()
+model.fit(train_X, train_y)
+val_predictions = model.predict(val_X)
+val_mae = mean_absolute_error(val_y, val_predictions)
+models[str(type(model))] = val_mae
 
 for k, v in models.items():
     print(f"{k}:{v}")
+
+"""
+<class 'sklearn.tree._classes.DecisionTreeRegressor'>:29228.96712328767
+<class 'sklearn.linear_model._base.LinearRegression'>:28589.835514103168
+<class 'sklearn.linear_model._ridge.Ridge'>:28588.983234742693
+<class 'sklearn.linear_model._coordinate_descent.Lasso'>:28589.722016086114
+<class 'sklearn.linear_model._coordinate_descent.ElasticNet'>:28736.657736926678
+<class 'sklearn.linear_model._least_angle.LassoLars'>:28586.297309171194
+<class 'sklearn.linear_model._bayes.BayesianRidge'>:28577.915718501103
+<class 'sklearn.linear_model._logistic.LogisticRegression'>:43482.191780821915
+<class 'sklearn.linear_model._glm.glm.GammaRegressor'>:57726.754047663686
+<class 'sklearn.linear_model._glm.glm.PoissonRegressor'>:57726.754047663686
+<class 'sklearn.linear_model._huber.HuberRegressor'>:32022.141050914273
+<class 'sklearn.linear_model._glm.glm.TweedieRegressor'>:34614.4810391937
+"""
